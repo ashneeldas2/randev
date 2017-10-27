40c01e9d3741d37f18665a97573d7c33c3578eb7
ashneeldas2 <adas2@stuy.edu>
2017-10-27 19:00:50 -0400

    added all functionality, needs makefile

--- randev.c
+++ randev.c
@@ -6,17 +6,39 @@
 #include <errno.h>
 #include <string.h>
 
-int * get_random() {
+int get_random() {
     int fd = open("/dev/random", O_RDONLY);
-    int * nums;
-    read(fd, nums, sizeof(int) * 10);
-    return nums;
+    int num;
+    read(fd, &num, sizeof(int));
+    if (errno) {
+        printf("Errno number: %d\n", errno);
+        printf("Message: %s\n", strerror(errno));
+    }
+    close(fd);
+    return num;
 }
 
 int main() {
-    int * arr = get_random();
-    int i = 0;
+    int arr[10];
+    int i = 0; 
     for (; i < 10; i++) {
+        arr[i] = get_random();
         printf("random %d: %d\n", i, arr[i]);
+    }
+
+    int fd = open("rand_nums", O_CREAT | O_RDWR, 0644);
+    printf("\nWriting numbers to file...\n");
+    write(fd, arr, sizeof(arr));
+    close(fd);
+
+    int nums[10];
+    fd = open("rand_nums", O_RDONLY, 0664);
+    printf("\nReading numbers from file...\n");
+    read(fd, nums, sizeof(nums));
+
+    printf("\nVerification that written values are the same:\n");
+    i = 0;
+    for (; i < 10; i++) {
+        printf("random %d: %d\n", i, nums[i]);
     }
 }

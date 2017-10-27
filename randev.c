7c4c942b021c3e33c563eef793d59795023ee39b
ashneeldas2 <adas2@stuy.edu>
2017-10-27 10:13:40 -0400

    added stuff

--- randev.c
+++ randev.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
-#include <fnctl.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
+int * get_random() {
+    int fd = open("/dev/random", O_RDONLY);
+    int * nums;
+    read(fd, nums, sizeof(int) * 10);
+    return nums;
+}
+
+int main() {
+    int * arr = get_random();
+    int i = 0;
+    for (; i < 10; i++) {
+        printf("random %d: %d\n", i, arr[i]);
+    }
+}
